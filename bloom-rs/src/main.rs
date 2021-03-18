71673008bba561beca6351fa9a3e46eb06167e2b
vii <vii@gamecraft.tech>
2021-03-18 09:14:30 +0100

    * move Bloom into a separate project

---
+++ bloom-rs/src/main.rs
@@ -0,0 +1,14 @@
+// I'm gonna go nuts from all this noise...
+#![allow(dead_code)]
+
+#[macro_use]
+extern crate nom;
+
+#[macro_use]
+extern crate enum_display_derive;
+
+mod parsing;
+
+fn main() {
+    println!("Hello, world!");
+}
